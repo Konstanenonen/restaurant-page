e90b7188b3313d11c3dc667ba2de20f60b792284
Konsta Nenonen <konstanenonen@protonmail.com>
2022-05-14 11:02:21 +0300

    Images now work and started to add class names for elements

--- src/modules/firstLoad.ts
+++ src/modules/firstLoad.ts
@@ -1,20 +1,34 @@
-import Burger from './burger.jpg';
+import Burger from '../images/burger.jpg';
 
 const page = (() => {
   const _root = document.getElementById("content");
 
   const _createElements = () => {
     const navigation = document.createElement("nav");
+    navigation.classList.add("navigation");
+
     const main = document.createElement("main");
+    main.classList.add("main");
+
     const footer = document.createElement("footer");
+    footer.classList.add("footer");
+
     const title = document.createElement("h1");
+    title.classList.add("title");
+
     const description = document.createElement("p");
+    description.classList.add("description");
+
     const burgerPicture = new Image();
-    burgerPicture.src = Burger;
+    burgerPicture.classList.add("header-image");
 
     title.innerText = "Classic Burgers";
     description.innerText = "This is the place to visit when you want some good ass burgers!"
+    burgerPicture.src = Burger;
+
+
     main.appendChild(title);
+    main.appendChild(burgerPicture);
     main.appendChild(description);
 
     return [
