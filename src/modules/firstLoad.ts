dfa6c109182c443bd25bf9c91f3016dadb4341bc
Konsta Nenonen <konstanenonen@protonmail.com>
2022-05-20 23:39:41 +0300

    Started styling the page, deleted unused code

--- src/modules/firstLoad.ts
+++ src/modules/firstLoad.ts
@@ -4,35 +4,35 @@ const page = ((initialMainContent) => {
   const _root = document.getElementById("content");
 
   const _createElements = () => {
-    const navigation = document.createElement("nav");
-    navigation.classList.add("navigation");
-
     const homeLink = document.createElement("button");
     homeLink.classList.add("navigation-button");
     homeLink.id = "home-button";
+    homeLink.innerText = "Home";
 
     const menuLink = document.createElement("button");
     menuLink.classList.add("navigation-button");
     menuLink.id = "menu-button";
+    menuLink.innerText = "Menu";
 
     const contactLink = document.createElement("button");
     contactLink.classList.add("navigation-button");
     contactLink.id = "contact-button";
+    contactLink.innerText = "Contact";
 
     const main = document.createElement("main");
     main.classList.add("main");
     main.id = "main";
 
+    const footerText = document.createElement("p");
+    footerText.classList.add("footer-text");
+    footerText.innerText = "Made by Konsta Nenonen";
+
     const footer = document.createElement("footer");
     footer.classList.add("footer");
+    footer.appendChild(footerText);
 
-    const title = document.createElement("h1");
-    title.classList.add("title");
-
-    homeLink.innerText = "Home";
-    menuLink.innerText = "Menu";
-    contactLink.innerText = "Contact";
-
+    const navigation = document.createElement("nav");
+    navigation.classList.add("navigation");
     navigation.appendChild(homeLink);
     navigation.appendChild(menuLink);
     navigation.appendChild(contactLink);
