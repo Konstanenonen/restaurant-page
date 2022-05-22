ad986a1896604d364a28a09d55ca39302955120d
Konsta Nenonen <konstanenonen@protonmail.com>
2022-05-22 10:16:43 +0300

    Added owner to contact page

--- src/modules/contact.ts
+++ src/modules/contact.ts
@@ -1,7 +1,9 @@
+import Chef from '../images/chef.jpg';
+
 const contact = (() => {
   const _getRoot = () => {
     return document.getElementById("main");
-  }
+  };
 
   const _createContactContent = () => {
     const title = document.createElement("h1");
@@ -12,10 +14,29 @@ const contact = (() => {
     title.classList.add("sub-title");
     ownerTitle.innerText = "The owner";
 
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const cardPicture = new Image();
+    cardPicture.classList.add("card-image");
+    cardPicture.src = Chef;
+
+    const cardTitle = document.createElement("h3");
+    cardTitle.classList.add("card-title");
+    cardTitle.innerText = "Burger Lover";
+
+    const text = document.createElement("p");
+    text.classList.add("card-text");
+    text.innerText = "phone: 01234321";
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardPicture);
+    card.appendChild(text);
+
     const parentDiv = document.createElement("div");
     parentDiv.classList.add("main-content");
 
-    const elementsArray = [title, ownerTitle];
+    const elementsArray = [title, ownerTitle, card];
     elementsArray.forEach((element) => parentDiv.appendChild(element));
 
     return parentDiv;
@@ -28,7 +49,7 @@ const contact = (() => {
   };
 
   return {
-    load
+    load,
   };
 })();
 
